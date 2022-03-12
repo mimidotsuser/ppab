414961a6c0a8bf479908b3b2ec19df1e8607d1c2
murage <murage@labskenya.com>
2022-03-12 17:27:49 +0300

    refactor: switch to anonymous class for migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,12 +14,30 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('status')->default(\App\Utils\UserUtils::PendingActivation);
+            $table->foreignId('role_id');
             $table->rememberToken();
+            $table->foreignId('created_by_id')->nullable();
+            $table->foreignId('updated_by_id')->nullable();
             $table->timestamps();
+
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('created_by_id')
+                ->on('users')
+                ->references('id')
+                ->restrictOnDelete();
+
+            $table->foreign('updated_by_id')
+                ->on('users')
+                ->references('id')
+                ->restrictOnDelete();
         });
     }
 
@@ -33,4 +50,4 @@ class CreateUsersTable extends Migration
     {
         Schema::dropIfExists('users');
     }
-}
+};
