15c06e99b28fa3e1f8065096ac183bad7b899782
murage <murage@labskenya.com>
2022-03-12 17:28:46 +0300

    feat: add roles and permissions table creation migrations

---
+++ database/migrations/2022_03_11_230212_create_role_permissions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role_permissions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('role_id')->constrained('roles');
+            $table->foreignId('permission_id')->constrained('permissions');
+            $table->foreignId('created_by_id')->nullable()->constrained('users')
+                ->restrictOnDelete();;
+            $table->foreignId('updated_by_id')->nullable()->constrained('users')
+                ->restrictOnDelete();;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('role_permissions');
+    }
+};
