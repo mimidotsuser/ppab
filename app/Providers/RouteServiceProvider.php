9f6f35d5ef763a4fa359f123cb4424cbfde80196
murage <murage@labskenya.com>
2022-03-12 18:03:28 +0300

    fix: remove api rate limit if already authenticated

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
@@ -57,7 +58,8 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+            return Auth::check() ? Limit::none() :
+                Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
         });
     }
 }
