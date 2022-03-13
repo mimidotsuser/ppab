5ca443cff3c09375a1609e485bb3f315b4410d72
murage <murage@labskenya.com>
2022-03-13 23:09:49 +0300

    refactor: customize email base templates

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,17 @@
+<tr>
+    <td>
+        <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0"
+               role="presentation">
+            <tr>
+                <td class="content-cell" align="center">
+                    {{ Illuminate\Mail\Markdown::parse($slot) }}
+                </td>
+            </tr>
+            <tr>
+                <td class="content-cell" align="center">
+                    Do not reply this email. This is a system generated mail.
+                </td>
+            </tr>
+        </table>
+    </td>
+</tr>
