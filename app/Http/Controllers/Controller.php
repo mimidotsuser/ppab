2fb3d451abfef1b0e8b533903b804e498b8d6ecb
murage <murage@labskenya.com>
2022-03-13 18:09:20 +0300

    feat: add query meta parser at root controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,30 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @param array $sortColumns
+     * @param array $relationships
+     * @param string $defaultSortColumn
+     * @return object
+     */
+    public function queryMeta(array  $sortColumns = ['created_at'],
+                              array  $relationships = ['createdBy'],
+                              string $defaultSortColumn = 'created_at'): object
+    {
+
+        /* deserialize sorting columns and direction */
+        $requestSortCols = explode(',', request('sort_by', $defaultSortColumn));
+
+        /* deserialize columns to lazy load */
+        $requestLazyLoad = explode(',', request('include', ''));
+
+        return (object)[
+            'include' => array_intersect($requestLazyLoad, $relationships),
+            'orderBy' => array_intersect($requestSortCols, $sortColumns),
+            'direction' => request('order', 'desc'),
+            'limit' => request('limit', 15),
+            'page' => request('page', 1)
+        ];
+    }
 }
